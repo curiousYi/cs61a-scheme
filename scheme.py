5f719e85a6024911579fbfd03faa46f7e8db53ed
Yi Chao <changingtimes@gmail.com>
2019-07-04 15:50:51 -0500

    Finished Problem 4

--- scheme.py
+++ scheme.py
@@ -147,7 +147,12 @@ class PrimitiveProcedure(Procedure):
             python_args.append(args.first)
             args = args.second
         # BEGIN PROBLEM 4
-        "*** YOUR CODE HERE ***"
+        if self.use_env:
+            python_args.append(env)
+        try:
+            return self.fn(*python_args)
+        except TypeError:
+            raise SchemeError('wrong number of argumetns')
         # END PROBLEM 4
 
 class UserDefinedProcedure(Procedure):
